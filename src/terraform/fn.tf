3971e13219c321026b9ad5ddc5b91a2ef4d73efc
Jakub-Lukac <jakub.lukac81@gmail.com>
2024-05-13 14:45:29 +0100

    random added v1

--- src/terraform/fn.tf
+++ src/terraform/fn.tf
@@ -1,6 +1,6 @@
 # storage account for functions
 resource "azurerm_storage_account" "functions" {
-  name                     = "st${var.application_name}${var.environment_name}"
+  name                     = "st${var.application_name}${var.environment_name}${random_string.name.result}"
   resource_group_name      = azurerm_resource_group.main.name
   location                 = azurerm_resource_group.main.location
   account_tier             = "Standard"
