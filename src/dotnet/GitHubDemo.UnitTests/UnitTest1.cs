131abe9f9daa1154ea721150bf1bc1fcc12454c8
Jakub-Lukac <jakub.lukac81@gmail.com>
2024-05-14 13:48:41 +0100

    unit test body added

--- src/dotnet/GitHubDemo.UnitTests/UnitTest1.cs
+++ src/dotnet/GitHubDemo.UnitTests/UnitTest1.cs
@@ -2,10 +2,17 @@ namespace GitHubDemo.UnitTests
 {
     public class UnitTest1
     {
+        // this trait allows us to execute this unit tests through our ci pipeline
+        // run: dotnet test --no-restore --verbosity normal --filter Category=Unit
+        [Trait("Category", "Unit")]
         [Fact]
         public void Test1()
         {
-
+            // maybe
+            int a = 2;
+            int b = 3;
+            int sum = a + b;
+            Assert.True(sum == 5);
         }
     }
 }
